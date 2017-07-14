32598fcaf8deb6e5746999ae163555ccf8a86b08
Darlan Caruso <dev.caruso@gmail.com>
2017-07-13 22:21:53 -0300

    Add files via upload

--- express.js
+++ express.js
@@ -3,7 +3,7 @@ var consign = require('consign');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static('./app'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
